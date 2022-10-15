5bdb0e8c44bd9e27cef8d51433f182e8e49386a2
Pratyaksha Newalkar <newalkarpratyaksha@gmail.com>
2022-10-15 21:48:08 +0530

    Created Helloword.cpp

---
+++ Hello World Programs/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        cout << "\nHello World";
+    }
+    return 0;
+}
