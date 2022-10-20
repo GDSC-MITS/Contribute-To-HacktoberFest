5394a08260cac03d17e09308b47be9903407b01e
dab07 <harshitsh0711@gmail.com>
2022-10-20 12:13:03 +0530

    add Pascal Triangle program in java

---
+++ Java Programs/PascalTriangle.java
@@ -0,0 +1,23 @@
+/*
+Rotate the array to the right by k steps, where k is non-negative.
+The solution takes O(1) Space Complexity
+ */
+
+public class PascalTriangle {
+    public static void main(String args[]) {
+        int[] nums = new int[] {1,2,3,4,5,6,7};
+        int k = 3;
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; ++i) {
+            arr[(i+k) % nums.length] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = arr[i];
+        }
+    }
+}
